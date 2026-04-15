fcf344d7886e5f9d9d5a6bd7d841dba3d72fa0d8
sandeep3932 <csandeepreddy563@gmail.com>
2026-04-15 17:46:07 +0530

    Delete day01_python/Assisted_practice_problems directory

--- day01_python/Assisted_practice_problems/p2_descriptive_variable_names.py
+++
@@ -1,4 +0,0 @@
-initial_points = 10
-bonus_points = 20
-total_points = initial_points + bonus_points
-print(f"value of total points is: {total_points}")
--- day01_python/Assisted_practice_problems/p3_embracing_modularization.py
+++
@@ -1,7 +0,0 @@
-def add(a, b):
-    return a + b
-
-initial_points = 10
-bonus_points = 20
-total_points = add(initial_points, bonus_points)
-print(f"total points are : {total_points}")
